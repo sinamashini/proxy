1497a04e1f81b71a5f18fddf0fa5a51396ad270a
sinamashini <54537775+sinamashini@users.noreply.github.com>
2021-09-14 14:36:20 +0430

    Update index.js

--- index.js
+++ index.js
@@ -1,70 +1,29 @@
+import express from 'express';
+import puppeteer from 'puppeteer';
+import absolutify from 'absolutify';
 
-// const
-//     socks5 = require('simple-socks'),
-//     server = socks5.createServer();
+const app = express();
 
-// // start listening!
-// server.listen(1080, '0.0.0.0', function () {
-//     console.log('SOCKS5 proxy server started on 0.0.0.0:1080');
-// });
+app.get('/', async (req, res) => {
+    try {
+        const { url } = req.query;
+        if (!url) {
+            res.status(403).send("please provide a url");
+        }
+        else {
+            const browser = await puppeteer.launch();
+            const page = await browser.newPage();
+            await page.goto(`https://${url}`);
 
-// server.on('handshake', function (socket) {
-//     console.log();
-//     console.log('------------------------------------------------------------');
-//     console.log('new socks5 client from %s:%d', socket.remoteAddress, socket.remotePort);
-// });
-
-// // When a reqest arrives for a remote destination
-// server.on('proxyConnect', function (info, destination) {
-//     console.log('connected to remote server at %s:%d', info.address, info.port);
-
-//     destination.on('data', function (data) {
-//         console.log(data.length);
-//     });
-// });
-
-// server.on('proxyData', function (data) {
-//     console.log(data.length);
-// });
-
-// // When an error occurs connecting to remote destination
-// server.on('proxyError', function (err) {
-//     console.error('unable to connect to remote server');
-//     console.error(err);
-// });
-
-// // When a request for a remote destination ends
-// server.on('proxyDisconnect', function (originInfo, destinationInfo, hadError) {
-//     console.log(
-//         'client %s:%d request has disconnected from remote server at %s:%d with %serror',
-//         originInfo.address,
-//         originInfo.port,
-//         destinationInfo.address,
-//         destinationInfo.port,
-//         hadError ? '' : 'no ');
-// });
-
-// // When a proxy connection ends
-// server.on('proxyEnd', function (response, args) {
-//     console.log('socket closed with code %d', response);
-//     console.log(args);
-//     console.log();
-// });
-
-var Agent = require('socks5-https-client/lib/Agent');
-
-request({
-    url: 'https://encrypted.google.com/',
-    strictSSL: true,
-    agentClass: Agent,
-    agentOptions: {
-        socksHost: 'https://sinaproxy1.herokuapp.com', // Defaults to 'localhost'.
-        socksPort: 1080, // Defaults to 1080.
-
-        // Optional credentials
-        socksUsername: 'sina',
-        socksPassword: 'mashini',
+            let document = await page.evaluate(() => document.documentElement.outerHTML);
+            document = absolutify(document, `?url=${url.split('/')[0]}`);
+            res.send(document)
+        }
+    } catch (err) {
+        res.status(500).send(err);
     }
-}, function (err, res) {
-    console.log(err || res.body);
-});
+})
+
+const port = process.env.PORT || 3001
+
+app.listen(port, () => console.log(`listening on port: ${port}`))
